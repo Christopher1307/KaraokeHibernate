3481aa92e5705b69435a65c68cd4b41e2a51ed5b
JonathanGRiego02 <jonathangrclases@gmail.com>
2025-01-27 13:44:27 +0000

    feat: login view implemented

--- src/main/java/aed/ui/KaraokeApp.java
+++ src/main/java/aed/ui/KaraokeApp.java
@@ -1,6 +1,7 @@
 package aed.ui;
 
 import aed.ui.controllers.LoginController;
+import atlantafx.base.theme.CupertinoDark;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,8 +12,9 @@ public class KaraokeApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scene scene = new Scene(loginController.getLoginRoot());
-
+        Application.setUserAgentStylesheet(new CupertinoDark().getUserAgentStylesheet());
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
